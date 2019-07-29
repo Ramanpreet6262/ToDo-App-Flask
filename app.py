733b25e07bd7a50c9591d5a934a0977946aae0d0
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-07-29 19:14:37 +0530

    create functionality complete

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)   #"__name__ is just referencing this file"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # '////' is absolute path and '///' is realtive path
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/') 
+        except:
+            return "There was an issue adding your task"
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
